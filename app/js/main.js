d7a70dd2162fb64f3ce0d7d86752f013ba33ff8a
Pahanahan <korgfd@yandex.ua>
2023-09-03 17:19:58 +0300

    added services-page

--- app/js/main.js
+++ app/js/main.js
@@ -4,10 +4,10 @@ $(function () {
     e.preventDefault();
 
     // const chanchedAnimal = document.changeAnimal;
-    // function change_Status() {
+    // function change_Status_next() {
     //   chanchedAnimal.buttonChengedAnimal.disabled = (chanchedAnimal.animal.checked) ? false : true;
     // }
-    // change_Status();
+    // change_Status_next();
 
 
     $('.questions__choice').addClass('questions__choice--disactive');
@@ -187,7 +187,6 @@ $(function () {
   });
 
 
-
   $('.btn-applic').on('click', function (e) {
     e.preventDefault();
     $('.application').removeClass('application--disactive');
@@ -214,38 +213,37 @@ $(function () {
   });
 
 
-  // $('.review__btn').on('click', function (e) {
-  //   e.preventDefault();
-  //   $('.review-popap').removeClass('review-popap--disactive');
-  // });
-  // $('.review-popap__btn-close').on('click', function (e) {
-  //   e.preventDefault();
-  //   $('.review-popap').addClass('review-popap--disactive');
-  // });
-  // $('.btn-application').on('click', function (e) {
-  //   e.preventDefault();
-  //   $('.thanks').removeClass('thanks--disactive');
-  //   $('.application').addClass('application--disactive');
-  //   $('.btn-applic').addClass('btn-applic--disactive');
-  //   $('.btn-applic').removeClass('btn-gold');
-  //   $('.btn-applic--disactive').attr("disabled", "disabled");
-  // });
-  // $('.thanks__btn').on('click', function (e) {
-  //   e.preventDefault();
-  //   $('.thanks').addClass('thanks--disactive');
-  // });
-  // $('.thanks__btn-close').on('click', function (e) {
-  //   e.preventDefault();
-  //   $('.thanks').addClass('thanks--disactive');
-  // });
+  $('.review__btn').on('click', function (e) {
+    e.preventDefault();
+    $('.review-popap').removeClass('review-popap--disactive');
+  });
+  $('.review-popap__btn-close').on('click', function (e) {
+    e.preventDefault();
+    $('.review-popap').addClass('review-popap--disactive');
+  });
+  $('.btn-review').on('click', function (e) {
+    e.preventDefault();
+    $('.thanks-review').removeClass('thanks-review--disactive');
+    $('.review-popap').addClass('review-popap--disactive');
+    $('.review__btn').addClass('review__btn--disactive');
+    $('.review__btn').removeClass('btn-gold');
+    $('.review__btn--disactive').attr("disabled", "disabled");
+  });
+  $('.thanks__btn').on('click', function (e) {
+    e.preventDefault();
+    $('.thanks-review').addClass('thanks-review--disactive');
+  });
+  $('.thanks__btn-close').on('click', function (e) {
+    e.preventDefault();
+    $('.thanks-review').addClass('thanks-review--disactive');
+  });
+
 
   $('.review-popap__star').rateYo({
     rating: 4,
     spacing: "5px",
     starWidth: '11px',
     fullStar: true,
-    normalFill: "#FFD232",
-    ratedFill: "#FFD232",
     starSvg: '<svg xmlns="http://www.w3.org/2000/svg" xmlns: xlink = "http://www.w3.org/1999/xlink" width="18px" height="16px" viewBox="0 0 18 16" version="1.1"> <g id="surface1"> <path d="M 9.902344 0.5625 C 9.738281 0.21875 9.386719 0 9.003906 0 C 8.617188 0 8.273438 0.21875 8.101562 0.5625 L 6.09375 4.695312 L 1.605469 5.359375 C 1.230469 5.414062 0.917969 5.679688 0.804688 6.039062 C 0.6875 6.398438 0.78125 6.792969 1.050781 7.058594 L 4.304688 10.28125 L 3.539062 14.835938 C 3.476562 15.210938 3.632812 15.589844 3.941406 15.8125 C 4.25 16.035156 4.660156 16.0625 4.996094 15.882812 L 9.007812 13.742188 L 13.015625 15.882812 C 13.351562 16.0625 13.761719 16.039062 14.070312 15.8125 C 14.382812 15.585938 14.539062 15.210938 14.476562 14.835938 L 13.703125 10.28125 L 16.960938 7.058594 C 17.226562 6.792969 17.324219 6.398438 17.207031 6.039062 C 17.085938 5.679688 16.777344 5.414062 16.402344 5.359375 L 11.914062 4.695312 Z M 9.902344 0.5625 "/></g></svg>'
   });
 
@@ -259,6 +257,11 @@ function change_Status() {
 change_Status();
 
 
+// const review = document.formReview;
+// function change_Status_review() {
+//   review.startReview.disabled = (review.nameReview.value && review.nameAnimalReview.value && review.startReview.checked) ? false : true;
+// }
+// change_Status_review();
 
 
 const accordeon = document.querySelector('.specialist__inner-content');
